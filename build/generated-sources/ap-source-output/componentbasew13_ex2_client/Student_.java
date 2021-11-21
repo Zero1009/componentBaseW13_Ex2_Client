d884b70181b8aa0d88d13cbeb06b54ee03dcd155
Theerakan Natethaworn <theerakanzero@gmail.com>
2021-11-21 23:42:17 +0700

    complete

---
+++ build/generated-sources/ap-source-output/componentbasew13_ex2_client/Student_.java
@@ -0,0 +1,15 @@
+package componentbasew13_ex2_client;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2021-11-21T23:01:07")
+@StaticMetamodel(Student.class)
+public class Student_ { 
+
+    public static volatile SingularAttribute<Student, String> name;
+    public static volatile SingularAttribute<Student, Double> gpa;
+    public static volatile SingularAttribute<Student, Integer> id;
+
+}
---
+++ src/componentbasew13_ex2_client/ComponentBaseW13_Ex2_client.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package componentbasew13_ex2_client;
+
+import java.util.List;
+
+/**
+ *
+ * @author Theerakan
+ */
+public class ComponentBaseW13_Ex2_client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      int std = insertStudent(61050959, "Supawich sukkanta", 3.00);
+      //String std = updateStudent(61050959, "Supawich sukkanta", 2.00);
+       //services.Student std = findStudentById(61050210);
+       //String std = removeStudent(61050959);
+       System.out.println(std);
+       
+        
+    }
+    
+    private static services.Student findStudentById(int id){
+        
+        services.StudentWebService_Service service = new services.StudentWebService_Service();
+        services.StudentWebService port = service.getStudentWebServicePort();
+        return port.findStudentById(id);
+    }
+    
+    private static int insertStudent(int id, String name, double gpa){
+        services.StudentWebService_Service service = new services.StudentWebService_Service();
+        services.StudentWebService port = service.getStudentWebServicePort();
+        return port.insertStudent(id, name, gpa);
+    }
+    
+    private static String updateStudent(int id, String name, double gpa){
+        services.StudentWebService_Service service = new services.StudentWebService_Service();
+        services.StudentWebService port = service.getStudentWebServicePort();
+        return port.updateStudent(id, name, gpa);
+    }
+    
+    private static String removeStudent(int id){
+        services.StudentWebService_Service service = new services.StudentWebService_Service();
+        services.StudentWebService port = service.getStudentWebServicePort();
+        return port.removeStudent(id);
+    }
+    
+   
+    
+    
+    
+}
